2e757c220e725602a6a9577c31f00f078dec3cde
Swedz <email@jonahdunham.com>
2017-03-31 16:01:07 -0400

    Update Bans.java

--- src/main/java/net/Swedz/ubl/Bans.java
+++ src/main/java/net/Swedz/ubl/Bans.java
@@ -62,107 +62,13 @@ public class Bans {
 		});
 	}
 	
-	//The way I'm doing the parsing seems a little annoying ... I'll try to see if there's a better way to do it in the future.
 	public static class Parser {
-		private static int getMonthIndex(String m) {
-			switch(m) {
-				case "January": return 1;
-				case "February": return 2;
-				case "March": return 3;
-				case "April": return 4;
-				case "May": return 5;
-				case "June": return 6;
-				case "July": return 7;
-				case "August": return 8;
-				case "September": return 9;
-				case "October": return 10;
-				case "November": return 11;
-				case "December": return 12;
-				default: return 0;
-			}
-		}
-		
-		private static String getMonthByIndex(int i) {
-			switch(i) {
-				case 1: return "January";
-				case 2: return "February";
-				case 3: return "March";
-				case 4: return "April";
-				case 5: return "May";
-				case 6: return "June";
-				case 7: return "July";
-				case 8: return "August";
-				case 9: return "September";
-				case 10: return "October";
-				case 11: return "November";
-				case 12: return "December";
-				default: return null;
-			}
-		}
-		
-		private static boolean isNumber(String num) {
-			try {
-				int id = Integer.parseInt(num);
-				return true;
-			} catch (Exception ex) {
-				return false;
-			}
-		}
-		
-		private static String formatDate(String date) {
-        	date = date.replace(".", ",");
-        	while(date.contains(" ") || date.contains(" "))
-        		date = date.replace(" ", ",").replace(" ", ",");
-        	
-        	String newDate = "";
-        	boolean firstNumber = true;
-        	for(String ld : date.split("")) {
-        		boolean isNumber = isNumber(ld);
-        		if(isNumber && firstNumber) {
-        			firstNumber = false;
-        			newDate += ","+ld;
-        		} else
-        			newDate += ld;
-        	} newDate = newDate.replace(",,", ",");
-        	
-        	return newDate;
-		}
-		
 		public static HashMap<String, String> parseBans(String data) throws IOException {
 	    	HashMap<String, String> bans = new HashMap<String, String>();
 	        CSVParser parser = CSVFormat.EXCEL.withHeader().parse(new StringReader(data));
 	        for(CSVRecord csvRecord : parser) {
-	        	String expiry = formatDate(csvRecord.get("Expiry Date"));
-	        	
 	        	try {
-		        	int eMonth = getMonthIndex(expiry.split(",")[0]);
-		        	int eDay = Integer.parseInt(expiry.split(",")[1]);
-		        	int eYear = 0;
-		        	try {
-		        		eYear = Integer.parseInt(expiry.split(",")[3]);
-		        	} catch (ArrayIndexOutOfBoundsException ex1) {
-		        		try {
-		        			eYear = Integer.parseInt(expiry.split(",")[2]);
-		        		} catch (ArrayIndexOutOfBoundsException ex2) {
-		        			eYear = eDay;
-		        			eDay = 1;
-		        		}
-		        	}
-		        	
-		        	Calendar cal = Calendar.getInstance();
-		        	cal.setTime(new Date());
-		        	int month = cal.get(Calendar.MONTH);
-		        	int day = cal.get(Calendar.DAY_OF_MONTH);
-		        	int year = cal.get(Calendar.YEAR);
-		        	
-		        	if(year > eYear)
-		        		continue;
-		        	if(month == eMonth && day >= eDay && year >= eYear)
-		        		continue;
-		        	if(month > eMonth && year >= eYear)
-		        		continue;
-		        	
-		            UUID uuid;
+		        	UUID uuid = null;
 		            try {
 		                uuid = UUID.fromString(csvRecord.get("UUID"));
 		            } catch (Exception e) {
@@ -171,7 +77,6 @@ public class Bans {
 		            	bans.put(uuid.toString(), csvRecord.get("Reason"));
 	        	} catch (Exception ex) {
 	        		System.out.println("[BungeeUBL]: Error occurred while reading the Ban List.");
-	        		System.out.println("[Debug]: " + expiry);
 	        		ex.printStackTrace();
 	        	}
 	        } return bans;
