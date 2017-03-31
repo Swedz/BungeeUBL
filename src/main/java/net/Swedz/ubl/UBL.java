70af1a3eb54e00a97fb857c4c1a36bb56261dbcf
Swedz <email@jonahdunham.com>
2017-03-31 14:33:27 -0400

    Create UBL.java

---
+++ src/main/java/net/Swedz/ubl/UBL.java
@@ -0,0 +1,17 @@
+package net.Swedz.ubl;
+
+import java.util.HashMap;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+public class UBL extends Command {
+	public UBL() {
+		super("ubl");
+	}
+
+	@Override
+	public void execute(CommandSender sender, String[] args) {
+		//TODO add the /ubl command, currently not really important.
+	}
+}
